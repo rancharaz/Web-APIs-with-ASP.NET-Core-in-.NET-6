569ccf9e78e27c70b495ded68df681369a856638
rancharaz <ranveer1990@hotmail.com>
2022-06-12 17:39:40 +0400

    Web Api ASP.NET core .Net 6

---
+++ HPlusSportApi/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace HPlusSportApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+
+        public virtual List<Product> Products { get; set; }
+
+
+    }
+}
---
+++ HPlusSportApi/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HPlusSportApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; } = String.Empty;
+
+        public string Name { get; set; } = String.Empty;
+
+        public string Description { get; set; } = String.Empty;
+
+        public decimal Price { get; set; }
+
+        public decimal IssAvailable { get; set; }    
+
+        public int CategoryId { get; set; }
+
+        [JsonIgnore]
+
+        public virtual Category Category { get; set; }  
+
+    }
+}
